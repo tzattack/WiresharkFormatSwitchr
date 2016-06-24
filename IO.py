9aed3efe2614ac3dafd06d8beab5c44a00a08746
tzattack <tzattack@gmail.com>
2016-06-24 15:26:08 +0800

    file io example

--- IO.py
+++ IO.py
@@ -1,2 +1,26 @@
-f = open('./test.txt','r+')
-print(f)
+import random
+import sys
+import os
+
+
+#Write file
+'''
+test_file = open("test.txt","wb")
+print(test_file.mode)
+print(test_file.name)
+test_file.write(bytes("Write me to the file\n", 'UTF-8'))
+test_file.close()
+'''
+
+#Read file
+'''
+test_file = open("test.txt", "r+")
+text_in_file = test_file.read()
+print(text_in_file)
+
+'''
+
+#Remove file
+'''
+os.remove(test.txt)
+'''
