19673fb07d0523512effd0804f882ddde1d4845c
tzattack <tzattack@gmail.com>
2016-06-27 10:24:19 +0800

    list all the file in the current directory and subdirectory

--- IO.py
+++ IO.py
@@ -1,26 +1,56 @@
 import random
 import sys
 import os
+import glob
 
-
-#Write file
 '''
+print(os.path.basename("/a/b/c.txt"))
+
+# Write file
 test_file = open("test.txt","wb")
 print(test_file.mode)
 print(test_file.name)
 test_file.write(bytes("Write me to the file\n", 'UTF-8'))
 test_file.close()
-'''
 
-#Read file
-'''
+# Read file
+
 test_file = open("test.txt", "r+")
 text_in_file = test_file.read()
 print(text_in_file)
 
 '''
 
-#Remove file
-'''
-os.remove(test.txt)
-'''
+# File list with txt files
+file_list = glob.glob("./*.txt")
+print("TXT file list: ")
+print(file_list)
+
+
+print("File and directory list: ")
+print(os.listdir("./"))
+
+
+i = 0
+for file_name in file_list:
+    file_name = os.path.basename(file_list[i])
+    test_file = open(file_name, "r+")
+    text_in_file = test_file.read()
+    print(text_in_file)
+    i += 1
+
+a = open("output.txt", "w")
+all_file_list = []
+for path, subdirs, files in os.walk(r'./'):
+    for filename in files:
+        f = os.path.join(path, filename)
+        a.write(str(f) + os.linesep)
+        all_file_list.append(str(f))
+
+print(all_file_list)
+
+
+
+
+
+
