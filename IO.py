d9035faa00359d4f533a527ba4c78f63356bb130
tzattack <tzattack@gmail.com>
2016-06-28 11:23:19 +0800

    run command lines with python and added format converting lines

--- IO.py
+++ IO.py
@@ -2,6 +2,9 @@ import random
 import sys
 import os
 import glob
+import subprocess
+
+from builtins import print
 
 '''
 print(os.path.basename("/a/b/c.txt"))
@@ -42,22 +45,97 @@ for file_name in file_list:
 
 # a = open("output.txt", "w")
 all_file_list = []
-for path, subdirs, files in os.walk(r'./'):
+for path, subdirectories, files in os.walk(r'.'):
     for filename in files:
         f = os.path.join(path, filename)
         # a.write(str(f) + os.linesep)
         all_file_list.append(str(f))
-
+print("All the files in directory and subdirectory: ")
 print(all_file_list)
+
+'''
+Text file capturing
+'''
 
 j = 0
 txt_file_list = []
-for txt_file in all_file_list:
+for txt_file_item in all_file_list:
     if all_file_list[j].find(".txt") is not -1:
-        print("true!")
-        print(all_file_list[j])
         txt_file = all_file_list[j]
-        txt_file_list.append(all_file_list[j])
+        txt_file_list.append(txt_file)
     j += 1
-
+print("All the txt file: ")
 print(txt_file_list)
+
+k = 0
+for text in txt_file_list:
+    # Get file name
+    txt_file_name = os.path.basename(txt_file_list[k])
+
+    # Get absolute directory from relative directory
+    script_dir = os.path.dirname(__file__)
+    rel_path = txt_file_list[k]
+    abs_file_path = os.path.join(script_dir, rel_path)
+
+    # Open files
+    text_file = open(abs_file_path, "r+")
+    text_in_file = text_file.read()
+
+    print("TEXT IN " + txt_file_name + ": ")
+    print(text_in_file)
+
+    # Write into new files
+    script_output_dir = os.path.dirname(__file__)
+    rel_output_path = "./output/"
+    abs_output_file_path = os.path.join(script_output_dir, rel_output_path)
+    text_output_file = open(abs_output_file_path + txt_file_name, "wb")
+    text_output_file.write(bytes(text_in_file, 'UTF-8'))
+    text_output_file.close()
+    print("Wrote into " + txt_file_name + " succeeded! \n\n")
+
+    k += 1
+
+'''
+pcapng file converting
+'''
+pcapng = 0
+pcapng_file_list = []
+for pcapng_file in all_file_list:
+    if all_file_list[pcapng].find(".pcapng") is not -1:
+        pcapng_file = all_file_list[pcapng]
+        pcapng_file_list.append(pcapng_file)
+    pcapng += 1
+print("All the pcapng file: ")
+print(pcapng_file_list)
+
+pcapng = 0
+for content in pcapng_file_list:
+    # Get file name
+    file_name = os.path.basename(pcapng_file_list[pcapng])
+    print(file_name)
+    subprocess.call(["echo", "Hello, world!"], shell=True)
+    subprocess.call(["editcap", "-F", "cap", file_name, "output.cap"], shell=True)
+    '''
+    # Get absolute directory from relative directory
+    script_dir = os.path.dirname(__file__)
+    rel_path = pcapng_file_list[pcapng]
+    abs_file_path = os.path.join(script_dir, rel_path)
+
+    # Open files
+    text_file = open(abs_file_path, "r+")
+    text_in_file = text_file.read()
+
+    print("*******Content IN " + file_name + ": ")
+    print(text_in_file)
+
+
+    # Write into new files
+    script_output_dir = os.path.dirname(__file__)
+    rel_output_path = "./output/"
+    abs_output_file_path = os.path.join(script_output_dir, rel_output_path)
+    text_output_file = open(abs_output_file_path + txt_file_name, "wb")
+    text_output_file.write(bytes(text_in_file, 'UTF-8'))
+    text_output_file.close()
+    print("Wrote into " + txt_file_name + " succeeded! \n\n")
+    '''
+    pcapng += 1
