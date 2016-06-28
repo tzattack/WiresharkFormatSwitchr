d5d347799cf189e003b1fb3842ba51dd7d60e13b
tzattack <tzattack@gmail.com>
2016-06-28 16:02:36 +0800

    complete converting format

--- IO.py
+++ IO.py
@@ -30,18 +30,21 @@ for content in pcapng_file_list:
     # Get file name
     file_name = os.path.basename(pcapng_file_list[counter])
     print(file_name)
-    subprocess.call(["echo", "Hello, world!"], shell=True)
+    subprocess.call(["echo", "Hello, world!"], shell=False)
     # change directory
-    current_directory = os.path.dirname(__file__)
+    current_directory = os.path.dirname(os.path.abspath("__file__"))
     print("Current directory is : " + current_directory)
     subprocess.call(["cd", current_directory], shell=True)
     # converting capture format
-    script_dir = os.path.dirname(__file__)
+    script_dir = os.path.dirname(os.path.abspath("__file__"))
     rel_path = file_name
     abs_file_path = os.path.join(script_dir, rel_path)
+    infile = pcapng_file_list[counter]
+    print("*******Infile directory: " + infile)
     abs_file_output_path = os.path.join(script_dir, ".\output\\")
-    print("File output: " + abs_file_output_path)
-    subprocess.call(["editcap", "-F", "cap", abs_file_path, abs_file_output_path + file_name + "_output.cap"],
-                    shell=True)
+    output_file_name = "./output/" + file_name + "_output.cap"
+    print(output_file_name)
+    subprocess.call(["editcap", "-F", "ngwsniffer_2_0", infile, output_file_name],
+                    shell=False)
 
     counter += 1
