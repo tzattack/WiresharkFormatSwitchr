c8a04d5fcc2fe7cfb6cdf34d3ada37a4eaefa0b7
tzattack <tzattack@gmail.com>
2016-07-04 10:39:08 +0800

    Reading files

--- Converter.py
+++ Converter.py
@@ -1,36 +1,79 @@
-class pcap_hdr_s:
-    magic_number = 0
-    version_major = 0
-    version_minor = 0
-    thiszone = 0
-    sigfigs = 0
-    snaplen = 0
-    network = 0
+import os
+import subprocess
+import PcapngDecoder
+
+'''
+Loading all file list
+'''
+all_file_list = []
+for path, subdirectories, files in os.walk(r'.'):
+    for filename in files:
+        file = os.path.join(path, filename)
+        all_file_list.append(str(file))
+print("All the files in directory and subdirectory: ")
+print(all_file_list)
+print("-------------------------------------------------------------")
+
+'''
+pcapng file converting
+'''
+counter = 0
+convert_file_list = []
+for convert_file in all_file_list:
+    if all_file_list[counter].find(".snoop") is not -1:
+        convert_file = all_file_list[counter]
+        convert_file_list.append(convert_file)
+    if all_file_list[counter].find(".pcap") is not -1:
+        convert_file = all_file_list[counter]
+        convert_file_list.append(convert_file)
+    if all_file_list[counter].find(".cap") is not -1:
+        convert_file = all_file_list[counter]
+        convert_file_list.append(convert_file)
+    if all_file_list[counter].find(".libpcap") is not -1:
+        convert_file = all_file_list[counter]
+        convert_file_list.append(convert_file)
+    if all_file_list[counter].find(".5vw") is not -1:
+        convert_file = all_file_list[counter]
+        convert_file_list.append(convert_file)
+    if all_file_list[counter].find(".txt") is not -1:
+        convert_file = all_file_list[counter]
+        convert_file_list.append(convert_file)
+    if all_file_list[counter].find(".ncf") is not -1:
+        convert_file = all_file_list[counter]
+        convert_file_list.append(convert_file)
+    counter += 1
+print("All the files to be converted: ")
+print(convert_file_list)
+print("=============================================================")
+
+counter = 0
+for content in convert_file_list:
+    # Get file name
+    file_name = os.path.basename(convert_file_list[counter])
+
+    # change directory
+    current_directory = os.path.dirname(os.path.abspath("__file__"))
+    subprocess.call(["cd", current_directory], shell=True)
+
+    # converting capture format
+    infile = convert_file_list[counter]
+    outfile = "./output/" + file_name + "_output.cap"
+    subprocess.call(["editcap", "-F", "ngwsniffer_2_0", infile, outfile],
+                    shell=False)
+    print("File <" + file_name + "> has been converted into Sniffer 2.00x format!")
+    print("*************************************************************")
+    counter += 1
+
+file_type = "file type"
 
 
-origin_file = open("test.pcapng", "rb")
-origin_file_content = origin_file.read()
+def decode(file_type):
+    if file_type == "txt":
+        decode_txt()
+    elif file_type == "pcapng":
+        PcapngDecoder.decode_pcapng()
+    return 0
 
-block_type_start = 0
-block_typr_end = 4
-block_type = origin_file_content[block_type_start:block_typr_end]
-print("$$ Block Type: " + str(block_type))
 
-block_total_length_start = 4
-block_total_length_end = 8
-block_total_length = origin_file_content[block_total_length_start:block_total_length_end]
-print("$$ Block Total Length: " + str(block_total_length))
-
-block_body_start = 8
-block_body_end = origin_file_content[8:32].find(block_total_length) + 8
-block_body = origin_file_content[block_body_start:block_body_end]
-print("$$ Block Body: " + str(block_body))
-
-block_total_length_2_start = block_body_end
-block_total_length_2_end = block_total_length_2_start + 4
-block_total_length_2 = origin_file_content[block_total_length_2_start:block_total_length_2_end]
-print("$$ Block Total Length: " + str(block_total_length_2))
-if block_total_length != block_total_length_2:
-    print("Package parse error!")
-else:
-    print("Package parse succeed!")
+def decode_txt():
+    return 0
---
+++ PcapngDecoder.py
@@ -0,0 +1,40 @@
+def decode_pcapng():
+    origin_file = open("test.pcapng", "rb")
+    origin_file_content = origin_file.read()
+
+    block_type_start = 0
+    block_type_end = 4
+    block_type = origin_file_content[block_type_start:block_type_end]
+    print("$$ Block Type: " + str(block_type))
+
+    block_total_length_start = 4
+    block_total_length_end = 8
+    block_total_length = origin_file_content[block_total_length_start:block_total_length_end]
+    length = block_total_length.decode("utf-8")
+    print("$$ Block Total Length: " + str(block_total_length))
+
+    block_body_start = 8
+    block_body_end = origin_file_content[8:32].find(block_total_length) + 8
+    block_body = origin_file_content[block_body_start:block_body_end]
+    print("$$ Block Body: " + str(block_body))
+
+    block_total_length_2_start = block_body_end
+    block_total_length_2_end = block_total_length_2_start + 4
+    block_total_length_2 = origin_file_content[block_total_length_2_start:block_total_length_2_end]
+    print("$$ Block Total Length: " + str(block_total_length_2))
+    if block_total_length != block_total_length_2:
+        print("Package parse error!")
+    else:
+        print("Package parse succeed!")
+
+    device_system = origin_file_content[112:142]
+    print("$$ Device System: " + str(device_system))
+
+    lenght_of_package = len(origin_file_content)
+    print(lenght_of_package)
+
+    end_of_package = lenght_of_package - 518
+    real_content = origin_file_content[:end_of_package]
+    print(real_content)
+
+    return 0
